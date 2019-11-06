ecbc49ec8179eac40e3985867afaf2208c96d715
Nick Fletcher <nickfletcher720@gmail.com>
2019-11-06 13:46:50 +1000

    major updates

---
+++ db/migrate/20191106033123_add_name_to_products.rb
@@ -0,0 +1,6 @@
+class AddNameToProducts < ActiveRecord::Migration[6.0]
+  def change
+    add_column :products, :name, :string
+    change_column :products, :price, :decimal, precision: 8, scale: 2
+  end
+end
