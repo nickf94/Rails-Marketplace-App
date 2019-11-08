948ba52252b36cc4e90b76faa5a7c917e5a37f0b
Nick Fletcher <nickfletcher720@gmail.com>
2019-11-08 14:49:01 +1000

    Added final touches and stripe

--- SRC/config/routes.rb
+++ SRC/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'shopping_carts/index', to: "shopping_carts#index"
   resources :shopping_cart
-  get "orders", to: "orders#new", as: "new_order"
+  get 'orders/complete', to: "orders#complete", as: 'orders_complete'
+  get 'orders/cancel'
+
+  get 'orders/:product_id', to: "orders#new", as: "new_order"
   resources :orders
   get 'products/index', to: "products#index"
   post ':user_id/products', to: "products#create", as: "create_product"
